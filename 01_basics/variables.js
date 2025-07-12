e388a1ecd795a1a8758cb525ba198687c277df4b
Ajeet33 <91239470+Ajeet33@users.noreply.github.com>
2025-07-12 03:17:43 +0000

    forst time commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const pi = 3.14; //here when const use always that variables will be constant we can't change in that code.
+let account_id = "32344";
+var noUse = "this time not use because choose only priority"
+var noUse = "when call these same name var so only lsat wal a hi lega"
+let account_status;//ek ye bhistyle hai use krne ka var its undefined
+
+console.log(pi);
+console.log(account_id);
+console.log(noUse);
+
+console.table([ pi, account_id, noUse,account_status]);
